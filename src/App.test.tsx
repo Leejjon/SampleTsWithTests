ec59a07712bd652e8024e9eeeb256577123028cf
Leejjon <leejjon@gmail.com>
2019-09-12 23:54:06 +0200

    Apparently it was an alias for test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders welcome message', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Learn React')).toBeInTheDocument();
+test('renders welcome message', () => {
+    const {getByText} = render(<App/>);
+    expect(getByText('Learn React')).toBeInTheDocument();
 });
+
